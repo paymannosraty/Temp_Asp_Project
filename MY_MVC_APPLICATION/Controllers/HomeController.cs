907fe86b2b94c9ae5ee6f662f56381bbc668749c
nosratipayman@gmail.com <nosratipayman@gmail.com>
2020-01-21 18:18:56 +0330

    Add Init Role & User For The First Run

--- MY_MVC_APPLICATION/Controllers/HomeController.cs
+++ MY_MVC_APPLICATION/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿namespace MY_MVC_APPLICATION.Controllers
+﻿using System.Linq;
+
+namespace MY_MVC_APPLICATION.Controllers
 {
 	public partial class HomeController : Infrastructure.BaseController
 	{
@@ -9,6 +11,49 @@
 		[System.Web.Mvc.HttpGet]
 		public virtual System.Web.Mvc.ActionResult Index()
 		{
+			///در صورتی که برانامه برای بار اول اجرا می شود
+			///به صورت پیش فرض درج می گردد User و یک role یک
+			var anyRole =
+							MyDatabaseContext.Roles
+							.Any();
+			if (anyRole == false)
+			{
+				try
+				{
+					//**** Role
+					Models.Role initRole =
+					new Models.Role
+					{
+						Name = "Admin",
+						IsActive = true,
+					};
+
+					MyDatabaseContext.Roles.Add(initRole);
+
+					//**** User
+					Models.User initUser =
+						new Models.User
+						{
+							Age = 27,
+							IsActive = true,
+							RoleId = initRole.Id,
+							Password = "123456789",
+							Description = "Nothing",
+							Username = "PaymanNosraty",
+							FullName = "Payman Nosraty",
+						};
+
+					MyDatabaseContext.Users.Add(initUser);
+
+					MyDatabaseContext.SaveChanges();
+				}
+				catch (System.Exception)
+				{
+					throw;
+				}
+
+			}
+
 			return View();
 		}
 	}
